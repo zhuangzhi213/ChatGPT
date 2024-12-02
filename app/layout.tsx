2bc0b103ad9da8286c85fe23f5255de416cfda65
zhuangzhi <1716571003@qq.com>
2024-12-02 13:44:18 +0800

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "琅環书生ChatGPT",
+  title: "琅環书生AI",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
     title: "琅環书生ChatGPT",
